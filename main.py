8e5ddba312993cdff1cf3fdefa7fbac85561ea20
Sufien Tout <sufientout@gmail.com>
2020-08-28 02:43:16 +0000

    Added Generating and Fetching Rows, Columns, and Blocks

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ def main():
         sys.exit(1)
     term = Terminal()
     board = [' ' if c == '.' else c for c in sys.argv[1]]
-    full = ['0' if c == '.' else c for c in sys.argv[1]]
+    #full = tuple('%.2i' % i for i in range(81))
+    #full = tuple('%i%i' % (i // 27, (i % 9) // 3) for i in range(81))
+    full = tuple('%.2i %i%i %i' % getInfo(i) for i in range(81))
     with term.fullscreen(), term.cbreak():
         display(term, '0001', tuple(board))
         while True:
             if DEBUG and not PREV:
-                display(term, '0001', tuple(full))
+                display(term, '0001', full, 6)
             elif not DEBUG and PREV:
                 display(term, '0001', tuple(board))
             line = readline(term)
@@ -30,8 +32,8 @@ def main():
             else:
                 pass
 
-def display(term, name, board):
-    p = lambda s, t: s.join([t * 3] * 3)
+def display(term, name, board, pad=0):
+    p = lambda s, t: s.join([t * (3 + pad)] * 3)
     pp = lambda s, t: s.join([t] * 3) 
     top = '╔' + pp('╦', p('╤', '═')) + '╗'
     numbers = ('║' + pp('│', ' %s ')) * 3 + '║'
@@ -68,6 +70,29 @@ def readline(term):
 def backspace(line = ' '):
     back = '\b \b' * len(line)
     print(back, end='', flush=True)
+
+def getInfo(value):
+    row, col = getCPoint(value)
+    block = getBlock(value)
+    return (value, row, col, block)
+
+def getCPoint(value):
+    return (value // 9, value % 9)
+
+def getBPoint(value):
+    return (value // 27, (value % 9) // 3)
+
+def getBlock(value):
+    return 3 * (value // 27) + (value % 9) // 3
+
+def genRow(r):
+    return tuple((r * 9) + i for i in range(9))
+
+def genCol(c):
+    return tuple(c + (i * 9) for i in range(9))
+
+def genBlock(b):
+    return tuple((i % 3) + (3 * (b % 3)) + ((i // 3) + (3 * (b // 3))) * 9 for i in range(9))
 
 if __name__ == '__main__':
     main()
